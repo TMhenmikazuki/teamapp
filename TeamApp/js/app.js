0b573f75b84fd1b1402b9e52a9b987c48d1a7153
t.kim <t.kim@tcmobile.jp>
2018-05-16 22:36:18 +0900

    folder deleted

--- TeamApp/js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-     $(".nav-link").click(function(){
-         $("#article").load(".../"+$(this).attr("page")+".html");
-         console.log("loaded");
-     });
-});
